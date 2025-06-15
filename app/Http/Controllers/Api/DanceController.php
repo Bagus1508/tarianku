705a019bd0a90604f6c2e37b4d18ac7c7c3c346c
Bagus1508 <101942448+Bagus1508@users.noreply.github.com>
2025-06-15 12:47:05 +0700

    fix

--- app/Http/Controllers/Api/DanceController.php
+++ app/Http/Controllers/Api/DanceController.php
@@ -4,16 +4,27 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\DanceResource;
+use App\Models\Categories;
 use App\Models\Dance;
 use Illuminate\Http\Request;
 
 class DanceController extends Controller
 {
-    public function index() 
+    public function index(Request $request)
     {
-        $dances = Dance::paginate(10);
+        if ($request->has('origin_id')) {
+            $dances = Dance::where('categories_id', $request->origin_id)->paginate(10);
+            $origin = Categories::find($request->origin_id)?->name;
+        } else {
+            $dances = Dance::paginate(10);
+            $origin = 'All';
+        }
 
-        return new DanceResource(true, 'List Data Tari', $dances);
+        return new DanceResource(true, 'List Data Tari', [
+            'origin_id' => $request->origin_id ?? null,
+            'origin' => $origin ?? null,
+            'data' => $dances,
+        ]);
     }
 
     public function show($id)
